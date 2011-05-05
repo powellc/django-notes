ee7c20eb641d47b9344271fbe38dd8bdaacedda5
Colin Powell <colin@onecardinal.com>
2011-05-05 01:04:43 -0400

    Adding a brief form.

--- notes/forms.py
+++ notes/forms.py
@@ -6,3 +6,7 @@ class NoteForm(ModelForm):
         model = Note
         fields=['content', 'markup', 'topic','date', 'public']
         
+class BriefNoteForm(ModelForm):
+    class Meta:
+        model = Note
+        fields=['content', 'topic']
